97d62a86ee3f29008bd7e60b851664ebf1cfc6fb
ishan <ishan365.ia@gmail.com>
2021-04-01 10:59:31 +0530

    add the colors for the light theme

---
+++ lua/solarized/solarized-light.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+function M.setup()
+	local highlight = {
+		base2 = '#073642',
+		red = '#dc322f',
+		green = '#859900',
+		yellow = '#b58900',
+		blue = '#268bd2',
+		magenta = '#d33682',
+		cyan = '#2aa198',
+		base02 = '#eee8d5',
+		base3 = '#002b36',
+		orange = '#cb4b16',
+		base1 = '#586e75',
+		base0 = '#657b83',
+		base00 = '#839496',
+		violet = '#6c71c4',
+		base01 = '#93a1a1',
+		base03 = '#fdf6e3',
+		back = '#fdf6e3'
+	}
+	return highlight
+end
+
+return M
