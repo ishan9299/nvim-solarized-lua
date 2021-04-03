ee72c005c4305c81c3d267b8c26bb57efc8a3fd3
ishan <ishan365.ia@gmail.com>
2021-04-03 09:44:01 +0530

    indentation

--- lua/solarized/solarized-low/solarized-dark.lua
+++ lua/solarized/solarized-low/solarized-dark.lua
@@ -1,26 +1,26 @@
 local M = {}
 
 function M.setup()
-    local highlight = {
-        base02 = '#073642',
-        back = '#073642',
-        red = '#dc322f',
-        green = '#859900',
-        yellow = '#b58900',
-        blue = '#268bd2',
-        magenta = '#d33682',
-        cyan = '#2aa198',
-        base2 = '#eee8d5',
-        base03 = '#002b36',
-        orange = '#cb4b16',
-        base01 = '#586e75',
-        base00 = '#657b83',
-        base0 = '#839496',
-        violet = '#6c71c4',
-        base1 = '#93a1a1',
-        base3 = '#fdf6e3'
-    }
-    return highlight
+	local highlight = {
+		base02 = '#073642',
+		back = '#073642',
+		red = '#dc322f',
+		green = '#859900',
+		yellow = '#b58900',
+		blue = '#268bd2',
+		magenta = '#d33682',
+		cyan = '#2aa198',
+		base2 = '#eee8d5',
+		base03 = '#002b36',
+		orange = '#cb4b16',
+		base01 = '#586e75',
+		base00 = '#657b83',
+		base0 = '#839496',
+		violet = '#6c71c4',
+		base1 = '#93a1a1',
+		base3 = '#fdf6e3'
+	}
+	return highlight
 end
 
 return M
