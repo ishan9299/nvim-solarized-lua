0967b0b0a3e02767b120563546ab0ca9a062de8d
ishan <ishan365.ia@gmail.com>
2021-05-10 12:31:01 +0530

    add 8-bit colors to solarized-flat

--- lua/solarized/solarized-flat/solarized-dark.lua
+++ lua/solarized/solarized-flat/solarized-dark.lua
@@ -2,23 +2,24 @@ local M = {}
 
 function M.setup()
 	local highlight = {
-		base02 = '#073642',
-		red = '#dc322f',
-		green = '#859900',
-		yellow = '#b58900',
-		blue = '#268bd2',
-		magenta = '#d33682',
-		cyan = '#2aa198',
-		base2 = '#eee8d5',
-		base03 = '#002b36',
-		back = '#002b36',
-		orange = '#cb4b16',
-		base01 = '#586e75',
-		base00 = '#657b83',
-		base0 = '#839496',
-		violet = '#6c71c4',
-		base1 = '#93a1a1',
-		base3   = '#fdf6e3'
+		none = {'none', 'none'},
+		base02 = {'#073642',15},
+		red = {'#dc322f',193},
+		green = {'#859900',133},
+		yellow = {'#b58900',171},
+		blue = {'#268bd2',56},
+		magenta = {'#d33682',188},
+		cyan = {'#2aa198',61},
+		base2 = {'#eee8d5',238},
+		base03 = {'#002b36',7},
+		back = {'#002b36',7},
+		orange = {'#cb4b16',182},
+		base01 = {'#586e75',92},
+		base00 = {'#657b83',105},
+		base0 = {'#839496',134},
+		violet = {'#6c71c4',111},
+		base1 = {'#93a1a1',150},
+		base3 = {'#fdf6e3',253}
 	}
 	return highlight
 end
