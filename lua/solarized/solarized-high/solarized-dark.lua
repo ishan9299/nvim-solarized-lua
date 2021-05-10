494bf33d0c1e5d23d001867eae2b91566198299c
ishan <ishan365.ia@gmail.com>
2021-05-10 13:56:11 +0530

    add 8-bit colors

--- lua/solarized/solarized-high/solarized-dark.lua
+++ lua/solarized/solarized-high/solarized-dark.lua
@@ -2,29 +2,25 @@ local M = {}
 
 function M.setup()
 	local highlight = {
-		base02 = '#073642',
-		red = '#dc322f',
-		green = '#859900',
-		yellow = '#b58900',
-		blue = '#268bd2',
-		magenta = '#d33682',
-		cyan = '#2aa198',
-		base1 = '#eee8d5',
-		base03 = '#002b36',
-		back = '#002b36',
-		orange = '#cb4b16',
-		base01 = '#657b83',
-		base00 = '#839496',
-		violet = '#6c71c4',
-		base0 = '#93a1a1',
-		base2 = '#fdf6e3',
-		base3 = '#fdf6e3'
+		base02  = {'#073642',15},
+		red     = {'#dc322f',193},
+		green   = {'#859900',133},
+		yellow  = {'#b58900',171},
+		blue    = {'#268bd2',56},
+		magenta = {'#d33682',188},
+		cyan    = {'#2aa198',61},
+		base1   = {'#eee8d5',238},
+		base03  = {'#002b36',7},
+		back    = {'#002b36',7},
+		orange  = {'#cb4b16',182},
+		base01  = {'#657b83',105},
+		base00  = {'#839496',134},
+		violet  = {'#6c71c4',111},
+		base0   = {'#93a1a1',150},
+		base2   = {'#fdf6e3',253},
+		base3   = {'#fdf6e3',253}
 	}
 	return highlight
 end
 
 return M
-
---[[
-%s/\#073642/colors.base02/g | %s/\#dc322f/colors.red/g | %s/\#859900/colors.green/g | %s/\#b58900/colors.yellow/g | %s/\#268bd2/colors.blue/g | %s/\#d33682/colors.magenta/g | %s/\#2aa198/colors.cyan/g | %s/\#eee8d5/colors.base1/g | %s/\#002b36/colors.base03/g | %s/\#cb4b16/colors.orange/g | %s/\#657b83/colors.base01/g | %s/\#839496/colors.base00/g | %s/\#6c71c4/colors.violet/g | %s/\#93a1a1/colors.base0/g | %s/\#fdf6e3/colors.base3/g
-]]
--- lua/solarized/solarized-low/solarized-light.lua
+++ lua/solarized/solarized-low/solarized-light.lua
@@ -2,23 +2,23 @@ local M = {}
 
 function M.setup()
 	local highlight = {
-		base2 = '#073642',
-		red = '#dc322f',
-		green = '#859900',
-		yellow = '#b58900',
-		blue = '#268bd2',
-		magenta = '#d33682',
-		cyan = '#2aa198',
-		base02 = '#eee8d5',
-		back = '#eee8d5',
-		base3 = '#002b36',
-		orange = '#cb4b16',
-		base1 = '#586e75',
-		base0 = '#657b83',
-		base00 = '#839496',
-		violet = '#6c71c4',
-		base01 = '#93a1a1',
-		base03 = '#fdf6e3'
+		base2   = {'#073642',15},
+		red     = {'#dc322f',193},
+		green   = {'#859900',133},
+		yellow  = {'#b58900',171},
+		blue    = {'#268bd2',56},
+		magenta = {'#d33682',188},
+		cyan    = {'#2aa198',61},
+		base02  = {'#eee8d5',238},
+		back    = {'#eee8d5',238},
+		base3   = {'#002b36',7},
+		orange  = {'#cb4b16',182},
+		base1   = {'#586e75',92},
+		base0   = {'#657b83',105},
+		base00  = {'#839496',134},
+		violet  = {'#6c71c4',111},
+		base01  = {'#93a1a1',150},
+		base03  = {'#fdf6e3',253}
 	}
 	return highlight
 end
